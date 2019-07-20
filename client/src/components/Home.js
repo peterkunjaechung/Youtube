09ffd8a0b1ba1b1108d384faee47626731c9ca85
Amanda Garcia <algarcian@outlook.com>
2019-07-20 11:19:23 -0600

    Changed the project name

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Header, } from 'semantic-ui-react';
 
 const Home = () => (
-  <Header as="h3" textAlign="center">Devise Auth App</Header>
+  <Header as="h3" textAlign="center">UToob</Header>
 )
 
 export default Home;
